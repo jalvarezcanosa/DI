48579afe5e9cfd538def02319ee5c00a957aac79
Jorge Álvarez Canosa <jalvarezc23@fpcoruna.afundacion.org>
2025-10-24 09:50:07 +0200

    Ejercicio9

--- sprint1tkinter/Ejercicio9.py
+++ sprint1tkinter/Ejercicio9.py
@@ -2,7 +2,10 @@ import tkinter as tk
 from tkinter import messagebox
 
 def mostrar_acerca_de():
-
+    messagebox.showinfo(
+        "Acerca de",
+        "Ejercicio 9 creado con Tkinter"
+    )
 
 root = tk.Tk()
 root.title("Ejercicio9")
@@ -12,8 +15,11 @@ root.config(menu=barra_menu)
 
 menu_archivo = tk.Menu(barra_menu)
 barra_menu.add_cascade(label="Archivo", menu=menu_archivo)
+menu_archivo.add_command(label="Abrir")
 menu_archivo.add_command(label="Salir", command=root.quit)
 
-menu_ayuda = tk.Menu(root)
+menu_ayuda = tk.Menu(barra_menu)
+barra_menu.add_cascade(label="Ayuda", menu=menu_ayuda)
+menu_ayuda.add_command(label="Acerca de", command=mostrar_acerca_de)
 
 root.mainloop()
