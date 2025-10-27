046976d67330e102930e36bd0e3d5ac94f67eecb
Jorge Álvarez Canosa <jalvarezc23@fpcoruna.afundacion.org>
2025-10-27 10:02:34 +0100

    Ejercicio12

--- sprint1tkinter/Ejercicio12.py
+++ sprint1tkinter/Ejercicio12.py
@@ -4,14 +4,15 @@ from tkinter import messagebox
 def add_usuario():
     nombre = entry.get()
     edad = scale_edad.get()
-    genero = genero.get()
+    genero = var_genero.get()
 
     usuario_info = f"Nombre: {nombre}, \nEdad: {edad}, \nGenero: {genero}"
     listbox.insert(tk.END, usuario_info)
 
 def eliminar_usuario():
-    selected = listbox.curselection()
-    selected.delete()
+    indices = listbox.curselection()
+    if indices:
+        listbox.delete(indices[0])
 
 root = tk.Tk()
 root.title("Ejercicio12")
@@ -22,27 +23,30 @@ entry.pack()
 scale_edad = tk.Scale(root, from_=0, to=100, orient="horizontal")
 scale_edad.pack()
 
+var_genero = tk.StringVar(value="Otro")
 
-tk.Radiobutton(root, text="Masculino").pack()
-tk.Radiobutton(root, text="Femenino").pack()
-tk.Radiobutton(root, text="Otro").pack()
+tk.Radiobutton(root, text="Masculino", variable=var_genero, value="Masculino").pack()
+tk.Radiobutton(root, text="Femenino", variable=var_genero, value="Femenino").pack()
+tk.Radiobutton(root, text="Otro", variable=var_genero, value="Otro").pack()
 
-boton_add = tk.Button(root, text="Añadir")
+boton_add = tk.Button(root, text="Añadir", command=add_usuario)
 boton_add.pack()
 
-boton_eliminar = tk.Button(root, text="Eliminar")
+boton_eliminar = tk.Button(root, text="Eliminar", command=eliminar_usuario)
 boton_eliminar.pack()
 
-listbox = tk.Listbox(root)
+frame_lista = tk.Frame(root)
+frame_lista.pack(expand=True, fill="both")
+
+scrollbar = tk.Scrollbar(frame_lista)
+scrollbar.pack(side="right", fill="y")
+
+listbox = tk.Listbox(frame_lista, yscrollcommand=scrollbar.set)
 listbox.pack()
 
-boton_salir = tk.Button(root, text="Salir")
+scrollbar.config(command=listbox.yview)
 
-barra_menu = tk.Menu(root)
-root.config(menu=barra_menu)
-
-menu = tk.Menu(barra_menu)
-barra_menu.add_cascade(label="Salir", menu=menu)
-menu.add_command(label="Salir", command=root.quit)
+boton_salir = tk.Button(root, text="Salir", command=root.destroy)
+boton_salir.pack()
 
 root.mainloop()
