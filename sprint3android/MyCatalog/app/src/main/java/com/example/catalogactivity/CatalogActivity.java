8d14a903db490847a059519dedc28c374c206960
Jorge Álvarez Canosa <jalvarezc23@fpcoruna.afundacion.org>
2025-11-11 14:46:58 +0100

    TODO: Ejercicio 5 fragments a medio acabar

--- sprint3android/MyCatalog/app/src/main/java/com/example/catalogactivity/CatalogActivity.java
+++ sprint3android/MyCatalog/app/src/main/java/com/example/catalogactivity/CatalogActivity.java
@@ -1,29 +1,23 @@
 package com.example.catalogactivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class CatalogActivity extends AppCompatActivity {
-    private Button navigateButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_catalog);
 
-        Button navigateButton = findViewById(R.id.navigate_button);
-        navigateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(CatalogActivity.this, DetailViewsActivity.class);
-                startActivity(intent);
-            }
-        });
+        NavHostFragment navHostFragment = (NavHostFragment)
+                getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        NavigationUI.setupWithNavController(bottomNav, navController);
     }
 }
